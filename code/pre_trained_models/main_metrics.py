414914e37f615021ee9afa4ceff3edf604f8eaf5
nicolas <nicolasdibot0.2@gmail.com>
2021-06-29 17:00:17 +0200

    update

--- code/pre_trained_models/main_metrics.py
+++ code/pre_trained_models/main_metrics.py
@@ -28,7 +28,7 @@ import sparsenesslib.high_level as hl
 PIL.Image.MAX_IMAGE_PIXELS = 30001515195151997
 478940                             
 #'CFD','SCUT-FBP','MART','JEN','SMALLTEST','BIGTEST'
-list_bdd = ['SCUT-FBP'] #"['CFD','MART','JEN','SCUT-FBP','SMALLTEST','BIGTEST']"
+list_bdd = ['BIGTEST'] #"['CFD','MART','JEN','SCUT-FBP','SMALLTEST','BIGTEST']"
 model_name = 'VGG16'  # 'vgg16, resnet (...)'
 #weights = 'vggface' #'imagenet','vggface'
 list_weights = ['vggface'] #['vggface','imagenet','vggplace']
